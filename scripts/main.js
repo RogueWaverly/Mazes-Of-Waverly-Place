bfc71116d2f8c42fad53695ffef68a57ecc3bd43
WeiverlyRoe <waverlyroeger@gmail.com>
2017-03-25 22:30:58 -0700

    Added digits.

--- scripts/main.js
+++ scripts/main.js
@@ -28,9 +28,12 @@ var cols = 5;
 var edgeCount = 0;
 
 var rowMinusButton = document.getElementById('rowMinusButton');
+rowMinusButton.disabled = true;
 var rowPlusButton = document.getElementById('rowPlusButton');
 var colMinusButton = document.getElementById('colMinusButton');
 var colPlusButton = document.getElementById('colPlusButton');
+
+var inputText = document.getElementById('inputText').value;
 
 var mazeButton = document.getElementById('mazeButton');
 
@@ -46,23 +49,35 @@ displayRowCol();
 
 rowMinusButton.onclick = function()
 {
-	if(rows > 0)
+	if(rows > 4)
 	{
 		rows--;
+		rowPlusButton.disabled = false;
 		displayRowCol();
+		if(rows === 4)
+			rowMinusButton.disabled = true;
 	}	
 }
 rowPlusButton.onclick = function()
 {
-	rows++;
-	displayRowCol();
+	if(rows < 30)
+	{
+		rows++;
+		rowMinusButton.disabled = false;
+		displayRowCol();
+		if(rows === 30)
+			rowPlusButton.disabled = true;
+	}
 }
 colMinusButton.onclick = function()
 {
-	if(cols > 0)
+	if(cols > 3)
 	{
 		cols--;
+		colPlusButton.disabled = false;
 		displayRowCol();
+		if(cols === 3)
+			colMinusButton.disabled = true;
 	}
 }
 colPlusButton.onclick = function()
@@ -70,7 +85,10 @@ colPlusButton.onclick = function()
 	if(cols < 30)
 	{
 		cols++;
+		colMinusButton.disabled = false;
 		displayRowCol();
+		if(cols === 30)
+			colPlusButton.disabled = true;
 	}
 }
 
@@ -84,9 +102,19 @@ var PlayerPos = {i:0, j:0};
 
 var isPlaying = false;
 
-function displayZeroError()
+function isInputValid()
 {
-	document.getElementById('mazeDiv').innerHTML = "<p id='zeroError'>Both Rows and Columns should be greater than 0.</p>";
+	inputText = document.getElementById('inputText').value;
+	if(inputText.length > 14)
+		return false;
+	for(var i=0; i<inputText.length; i++)
+		if(inputText.charAt(i) > '9' || inputText.charAt(i) < '0')
+			return false;
+	return true;
+}
+function displayInputError()
+{
+	document.getElementById('mazeDiv').innerHTML = "<p class='error'>Input should only contain digits and have at most 14 characters.</p>";
 }
 
 // Display Maze
@@ -101,7 +129,6 @@ function displayMaze()
 	}
 	document.getElementById('mazeDiv').innerHTML += "<p id='end'>End</p>";
 }
-
 function updateHRow(row)
 {
 	MazeArray[2*row] = "<p class='mazeRow'>";
@@ -111,18 +138,21 @@ function updateHRow(row)
 		MazeArray[2*row] += "+";
 		if(HWallArray[row][i] === 1)
 			MazeArray[2*row] += "&mdash;&mdash;";
+		else if(HWallArray[row][i] === 2)
+			MazeArray[2*row] += "==";
 		else
 			MazeArray[2*row] += "&nbsp;&nbsp;";
 	}
 	MazeArray[2*row] += "+</p>";
 }
-
 function updateVRow(row)
 {
 	MazeArray[2*row+1] = "<p class='mazeRow'>";
 	
 	if(VWallArray[row][0] === 1)
 		MazeArray[2*row+1] += "|";
+	else if(VWallArray[row][0] === 2)
+		MazeArray[2*row+1] += "&#8214;";
 	else
 		MazeArray[2*row+1] += "&nbsp;";
 	for(var i=1; i<cols+1; i++)
@@ -133,6 +163,8 @@ function updateVRow(row)
 			MazeArray[2*row+1] += "&nbsp;&nbsp;";
 		if(VWallArray[row][i] === 1)
 			MazeArray[2*row+1] += "|";
+		else if(VWallArray[row][i] === 2)
+			MazeArray[2*row+1] += "&#8214;";
 		else
 			MazeArray[2*row+1] += "&nbsp;";
 	}
@@ -143,6 +175,12 @@ function updateVRow(row)
 
 function initMaze()
 {
+	if(cols < 2*inputText.length+1)
+	{
+		cols = 2*inputText.length+1;
+		displayRowCol();
+	}
+
 	MazeArray = new Array(2*rows+1);
 	for(var i=0; i<2*rows+1; i++)
 		MazeArray[i] = "";
@@ -155,7 +193,6 @@ function initMaze()
 			HWallArray[i][j] = 1;
 		updateHRow(i);
 	}
-
 	VWallArray = new Array(rows);
 	for(var i=0; i<rows; i++)
 	{
@@ -204,7 +241,6 @@ function findParent(p)
 		parent = {i: PointsArray[parent.i][parent.j].parA, j: PointsArray[parent.i][parent.j].parB};
 	return parent;
 }
-
 function joinSets(a, b)
 {
 	if(PointsArray[a.i][a.j].rank < PointsArray[b.i][b.j].rank)
@@ -225,7 +261,106 @@ function joinSets(a, b)
 	}
 }
 
-function decideToBuild(edgeNum)
+function initInput()
+{
+	var r0 = Math.floor(rows/2)-1;
+	var r1 = r0+1;
+	var r2 = r1+1;
+	for(var i=0; i<inputText.length; i++)
+	{
+		var c0 = Math.floor((cols-2*inputText.length-1)/2)+1+2*i;
+		var c1 = c0+1;
+		var c2 = c1+1;
+		switch(inputText.charAt(i))
+		{
+			case '0':
+				HWallArray[r0][c0] = 2;	// top HWall
+				HWallArray[r2][c0] = 2;	// bot HWall
+
+				VWallArray[r0][c0] = 2;	// topL VWall
+				VWallArray[r0][c1] = 2;	// topR VWall
+				VWallArray[r1][c0] = 2;	// botL VWall
+				VWallArray[r1][c1] = 2;	// botR VWall
+				break;
+			case '1':
+				VWallArray[r0][c1] = 2;	// topR VWall
+				VWallArray[r1][c1] = 2;	// botR VWall
+				break;
+			case '2':
+				HWallArray[r0][c0] = 2;	// top HWall
+				HWallArray[r1][c0] = 2;	// mid HWall
+				HWallArray[r2][c0] = 2;	// bot HWall
+
+				VWallArray[r0][c1] = 2;	// topR VWall
+				VWallArray[r1][c0] = 2;	// botL VWall
+				break;
+			case '3':
+				HWallArray[r0][c0] = 2;	// top HWall
+				HWallArray[r1][c0] = 2;	// mid HWall
+				HWallArray[r2][c0] = 2;	// bot HWall
+
+				VWallArray[r0][c1] = 2;	// topR VWall
+				VWallArray[r1][c1] = 2;	// botR VWall
+				break;
+			case '4':
+				HWallArray[r1][c0] = 2;	// mid HWall
+
+				VWallArray[r0][c0] = 2;	// topL VWall
+				VWallArray[r0][c1] = 2;	// topR VWall
+				VWallArray[r1][c1] = 2;	// botR VWall
+				break;
+			case '5':
+				HWallArray[r0][c0] = 2;	// top HWall
+				HWallArray[r1][c0] = 2;	// mid HWall
+				HWallArray[r2][c0] = 2;	// bot HWall
+
+				VWallArray[r0][c0] = 2;	// topL VWall
+				VWallArray[r1][c1] = 2;	// botR VWall
+				break;
+			case '6':
+				HWallArray[r0][c0] = 2;	// top HWall
+				HWallArray[r1][c0] = 2;	// mid HWall
+				HWallArray[r2][c0] = 2;	// bot HWall
+
+				VWallArray[r0][c0] = 2;	// topL VWall
+				VWallArray[r1][c0] = 2;	// botL VWall
+				VWallArray[r1][c1] = 2;	// botR VWall
+				break;
+			case '7':
+				HWallArray[r0][c0] = 2;	// top HWall
+
+				VWallArray[r0][c1] = 2;	// topR VWall
+				VWallArray[r1][c1] = 2;	// botR VWall
+				break;
+			case '8':
+				HWallArray[r0][c0] = 2;	// top HWall
+				HWallArray[r1][c0] = 2;	// mid HWall
+				HWallArray[r2][c0] = 2;	// bot HWall
+
+				VWallArray[r0][c0] = 2;	// topL VWall
+				VWallArray[r0][c1] = 2;	// topR VWall
+				VWallArray[r1][c0] = 2;	// botL VWall
+				VWallArray[r1][c1] = 2;	// botR VWall
+				break;
+			case '9':
+				HWallArray[r0][c0] = 2;	// top HWall
+				HWallArray[r1][c0] = 2;	// mid HWall
+				HWallArray[r2][c0] = 2;	// bot HWall
+
+				VWallArray[r0][c0] = 2;	// topL VWall
+				VWallArray[r0][c1] = 2;	// topR VWall
+				VWallArray[r1][c1] = 2;	// botR VWall
+				break;
+		}
+	}
+	updateHRow(r0);
+	updateHRow(r1);
+	updateHRow(r2);
+	updateVRow(r0);
+	updateVRow(r1);
+}
+
+function isAvail(edgeNum)
 {
 	var numOfHWalls = (rows-1)*cols;
 	var i, j;
@@ -234,48 +369,74 @@ function decideToBuild(edgeNum)
 	{
 		i = Math.floor(edgeNum/cols)+1;
 		j = edgeNum%cols;
-		var topSetParent = findParent(PointsArray[i-1][j]);
-		var botSetParent = findParent(PointsArray[i][j]);
-		if(topSetParent.i !== botSetParent.i || topSetParent.j !== botSetParent.j)
-		{
-			joinSets(topSetParent, botSetParent);
-			HWallArray[i][j] = 0;
-			updateHRow(i);
-		}
+		if(HWallArray[i][j] === 2)
+			return false;
 	}
 	else						// VWall
 	{
 		edgeNum -= numOfHWalls;
 		i = edgeNum%rows;
 		j = Math.floor(edgeNum/rows)+1;
-		var leftSetParent = findParent(PointsArray[i][j-1]);
-		var rightSetParent = findParent(PointsArray[i][j]);
-		if(leftSetParent.i !== rightSetParent.i || leftSetParent.j !== rightSetParent.j)
+		if(VWallArray[0][0] === 2)
+			return false;
+	}
+	return true;
+}
+function decideToBuild(edgeNum)
+{
+	var numOfHWalls = (rows-1)*cols;
+	var i, j;
+
+	if(isAvail(edgeNum))
+	{
+		if(edgeNum < numOfHWalls)	// HWall
 		{
-			joinSets(leftSetParent, rightSetParent);
-			VWallArray[i][j] = 0;
-			updateVRow(i);
+			i = Math.floor(edgeNum/cols)+1;
+			j = edgeNum%cols;
+			var topSetParent = findParent(PointsArray[i-1][j]);
+			var botSetParent = findParent(PointsArray[i][j]);
+			if(HWallArray[i][j] !== 2 && (topSetParent.i !== botSetParent.i || topSetParent.j !== botSetParent.j))
+			{
+				joinSets(topSetParent, botSetParent);
+				HWallArray[i][j] = 0;
+				updateHRow(i);
+			}
+		}
+		else						// VWall
+		{
+			edgeNum -= numOfHWalls;
+			i = edgeNum%rows;
+			j = Math.floor(edgeNum/rows)+1;
+			var leftSetParent = findParent(PointsArray[i][j-1]);
+			var rightSetParent = findParent(PointsArray[i][j]);
+			if(VWallArray[i][j] !== 2 && (leftSetParent.i !== rightSetParent.i || leftSetParent.j !== rightSetParent.j))
+			{
+				joinSets(leftSetParent, rightSetParent);
+				VWallArray[i][j] = 0;
+				updateVRow(i);
+			}
 		}
 	}
 }
 
 mazeButton.onclick = function()
 {
-	if(rows > 0 && cols > 0)
+	if(!isInputValid())
+	{
+		displayInputError();
+		isPlaying = false;
+	}
+	else
 	{
 		isPlaying = true;
 		initMaze();
 		makeExits();
 		shuffleExits();
+		initInput();
 		shuffleArray(EdgeIndexArray);
 		for(var i=0; i<EdgeIndexArray.length; i++)
 			decideToBuild(EdgeIndexArray[i]);
 		displayMaze();
-	}
-	else
-	{
-		displayZeroError();
-		isPlaying = false;
 	}
 }
 
@@ -284,6 +445,7 @@ mazeButton.onclick = function()
 window.onkeyup = function(e)
 {
 	var key = e.keyCode ? e.keyCode : e.which;
+	e.preventDefault();
 	if(isPlaying)
 		switch(key)
 		{
@@ -303,6 +465,7 @@ window.onkeyup = function(e)
 			case 38:			// W - up
 			{
 				e.preventDefault();
+				e.stopImmediatePropagation()
 				if(PlayerPos.i > 0 && HWallArray[PlayerPos.i][PlayerPos.j] === 0)
 				{
 					PlayerPos.i--;
@@ -328,6 +491,7 @@ window.onkeyup = function(e)
 			case 40:			// S - down
 			{
 				e.preventDefault();
+				e.stopImmediatePropagation()
 				if(HWallArray[PlayerPos.i+1][PlayerPos.j] === 0)
 				{
 					PlayerPos.i++;
